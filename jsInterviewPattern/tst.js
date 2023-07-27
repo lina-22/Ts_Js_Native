40c4e57aa8e8f49677a3c55a58e72790a179791f
lina-22 <haquefahinur@gmail.com>
2023-07-27 09:29:40 +0200

    hoisting, scoping , shadowing and more

---
+++ jsInterviewPattern/tst.js
@@ -0,0 +1,24 @@
+// function f1() {
+//   console.log("test var ", y);
+//   console.log("test let ", x);
+// }
+// const f2 = () => {};
+// f1();
+// var y;
+// let x;
+
+// event loop
+// hoisting
+// scope
+// execution context
+// js engine
+// es 6
+// how many loops
+// object
+// function arrow function
+
+let x = (age) => {
+  if (age > 60) return "ok";
+  if (age > 30) return "ok";
+  return "not ok";
+};
